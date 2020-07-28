e4a55351fd238e1861a0f6556d9aafbfc4f7e58a
KonstantinMinsk <Sokolov.k007@yandex.ru>
2020-07-28 11:45:45 +0300

    feat: add js event for button like heart

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # JS---Creating-elements
 Глава 1: Условия и создание элементов
+
+likes.js - реализация функционала для лайков add js event for button like heart
--- src/likes.js
+++ src/likes.js
@@ -1,5 +1,8 @@
 let heart = document.querySelector('.heart');
+let likesNumber = document.querySelector('.likes-number');
 
 heart.onclick = function () {
+  // console.log(heart.classList.contains('added'));
+  heart.classList.contains('added') ? likesNumber.textContent-- : likesNumber.textContent++;
   heart.classList.toggle('added');
 };
